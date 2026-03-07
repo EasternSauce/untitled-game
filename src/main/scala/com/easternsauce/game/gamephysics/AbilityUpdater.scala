3a316f7e92cc2c7e80faade90ab19994ed1c9244
EasternSauce <frequentmerger@gmail.com>
2026-03-07 14:05:57 +0100

    remove synchronizers

--- src/main/scala/com/easternsauce/game/gamephysics/AbilityUpdater.scala
+++ src/main/scala/com/easternsauce/game/gamephysics/AbilityUpdater.scala
@@ -11,80 +11,71 @@ case class AbilityUpdater() {
 
   private var abilityBodies: mutable.Map[GameEntityId[AbilityComponent], AbilityBody] = _
   private var areaWorlds: mutable.Map[AreaId, AreaWorld] = _
-  private var synchronizer: AbilityBodySynchronizer = _
 
   def init(
-      abilityBodies: mutable.Map[GameEntityId[AbilityComponent], AbilityBody],
-      areaWorlds: mutable.Map[AreaId, AreaWorld]
-  ): Unit = {
+            abilityBodies: mutable.Map[GameEntityId[AbilityComponent], AbilityBody],
+            areaWorlds: mutable.Map[AreaId, AreaWorld]
+          ): Unit = {
     this.abilityBodies = abilityBodies
     this.areaWorlds = areaWorlds
-    synchronizer = AbilityBodySynchronizer()
-    synchronizer.init(abilityBodies, areaWorlds)
   }
 
   def update(areaId: AreaId)(implicit game: CoreGame): Unit = {
     abilityBodies
-      .filter { case (abilityId, _) =>
-        game.gameState.abilityComponents(abilityId).params.currentAreaId == areaId
+      .filter { case (id, _) =>
+        game.gameState.abilityComponents(id).params.currentAreaId == areaId
       }
       .values
       .foreach(_.update(game.gameState))
   }
 
-  def synchronize(areaId: AreaId)(implicit game: CoreGame): Unit =
-    synchronizer.synchronize(areaId)
+  def synchronize(areaId: AreaId)(implicit game: CoreGame): Unit = {
+    val ents = game.gameState.abilityComponents
+
+    val toCreate = ents.values.filter(e =>
+      !abilityBodies.contains(e.id) && e.params.currentAreaId == areaId
+    )
+
+    val toDestroy = abilityBodies.values.filter(b =>
+      !ents.contains(b.abilityComponentId) || b.areaId != areaId
+    )
+
+    toCreate.foreach { e =>
+      val body = AbilityBody(e.id)
+      body.init(areaWorlds(e.params.currentAreaId), e.pos)
+      abilityBodies.update(e.id, body)
+    }
+
+    toDestroy.foreach { b =>
+      b.onRemove()
+      abilityBodies.remove(b.abilityComponentId)
+    }
+  }
 
   def correctPositions(areaId: AreaId)(implicit game: CoreGame): Unit = {
-    game.gameState.abilityComponents.values.foreach(ability =>
+    game.gameState.abilityComponents.values.foreach { ability =>
       if (
         ability.params.currentAreaId == areaId &&
-        abilityBodies.contains(ability.id) &&
-        abilityBodies(ability.id).pos
-          .distance(ability.pos) > Constants.PhysicsBodyCorrectionDistance
+          abilityBodies.contains(ability.id) &&
+          abilityBodies(ability.id).pos.distance(ability.pos) > Constants.PhysicsBodyCorrectionDistance
       ) {
         abilityBodies(ability.id).setPos(ability.pos)
       }
-    )
-  }
-
-  def setBodyPosIfInArea(
-      abilityId: GameEntityId[AbilityComponent],
-      pos: Vector2f,
-      areaId: AreaId,
-      game: CoreGame
-  ): Unit = {
-    if (
-      game.gameState.abilityComponents.contains(abilityId) &&
-      game.gameState.abilityComponents(abilityId).params.currentAreaId == areaId
-    ) {
-      abilityBodies(abilityId).setPos(pos)
     }
   }
 
-  def setSensorIfInArea(
-      abilityId: GameEntityId[AbilityComponent],
-      areaId: AreaId,
-      game: CoreGame
-  ): Unit = {
-    if (
-      game.gameState.abilityComponents.contains(abilityId) &&
-      game.gameState.abilityComponents(abilityId).params.currentAreaId == areaId
-    ) {
-      abilityBodies(abilityId).setSensor()
+  def setBodyPosIfInArea(id: GameEntityId[AbilityComponent], pos: Vector2f, areaId: AreaId, game: CoreGame): Unit =
+    if (game.gameState.abilityComponents.get(id).exists(_.params.currentAreaId == areaId)) {
+      abilityBodies(id).setPos(pos)
     }
-  }
 
-  def setNonSensorIfInArea(
-      abilityId: GameEntityId[AbilityComponent],
-      areaId: AreaId,
-      game: CoreGame
-  ): Unit = {
-    if (
-      game.gameState.abilityComponents.contains(abilityId) &&
-      game.gameState.abilityComponents(abilityId).params.currentAreaId == areaId
-    ) {
-      abilityBodies(abilityId).setNonSensor()
+  def setSensorIfInArea(id: GameEntityId[AbilityComponent], areaId: AreaId, game: CoreGame): Unit =
+    if (game.gameState.abilityComponents.get(id).exists(_.params.currentAreaId == areaId)) {
+      abilityBodies(id).setSensor()
     }
-  }
+
+  def setNonSensorIfInArea(id: GameEntityId[AbilityComponent], areaId: AreaId, game: CoreGame): Unit =
+    if (game.gameState.abilityComponents.get(id).exists(_.params.currentAreaId == areaId)) {
+      abilityBodies(id).setNonSensor()
+    }
 }
--- src/main/scala/com/easternsauce/game/gamephysics/CreatureUpdater.scala
+++ src/main/scala/com/easternsauce/game/gamephysics/CreatureUpdater.scala
@@ -8,82 +8,74 @@ import com.easternsauce.game.math.Vector2f
 import scala.collection.mutable
 
 case class CreatureUpdater() {
+
   private var creatureBodies: mutable.Map[GameEntityId[Creature], CreatureBody] = _
   private var areaWorlds: mutable.Map[AreaId, AreaWorld] = _
-  private var synchronizer: CreatureBodySynchronizer = _
 
   def init(
-      creatureBodies: mutable.Map[GameEntityId[Creature], CreatureBody],
-      areaWorlds: mutable.Map[AreaId, AreaWorld]
-  ): Unit = {
+            creatureBodies: mutable.Map[GameEntityId[Creature], CreatureBody],
+            areaWorlds: mutable.Map[AreaId, AreaWorld]
+          ): Unit = {
     this.creatureBodies = creatureBodies
     this.areaWorlds = areaWorlds
-    synchronizer = CreatureBodySynchronizer()
-    synchronizer.init(creatureBodies, areaWorlds)
   }
 
   def update(areaId: AreaId)(implicit game: CoreGame): Unit = {
     creatureBodies
-      .filter { case (creatureId, _) =>
-        game.gameState.creatures(creatureId).params.currentAreaId == areaId
+      .filter { case (id, _) =>
+        game.gameState.creatures(id).params.currentAreaId == areaId
       }
       .values
       .foreach(_.update(game.gameState))
   }
 
-  def synchronize(areaId: AreaId)(implicit game: CoreGame): Unit =
-    synchronizer.synchronizeForArea(areaId)
+  def synchronize(areaId: AreaId)(implicit game: CoreGame): Unit = {
+    val ents = game.gameState.creatures
+
+    val toCreate = ents.values.filter(c =>
+      !creatureBodies.contains(c.id) && c.params.currentAreaId == areaId
+    )
+
+    val toDestroy = creatureBodies.values.filter(b =>
+      !ents.contains(b.creatureId) || b.areaId != areaId
+    )
+
+    toCreate.foreach { c =>
+      val body = CreatureBody(c.id)
+      body.init(areaWorlds(c.params.currentAreaId), c.pos)
+      creatureBodies.update(c.id, body)
+    }
+
+    toDestroy.foreach { b =>
+      b.onRemove()
+      creatureBodies.remove(b.creatureId)
+    }
+  }
 
   def correctPositions(areaId: AreaId)(implicit game: CoreGame): Unit = {
-    game.gameState.creatures.values.foreach(creature =>
+    game.gameState.creatures.values.foreach { c =>
       if (
-        creature.params.currentAreaId == areaId &&
-        creatureBodies.contains(creature.id) &&
-        creatureBodies(creature.id).pos
-          .distance(creature.pos) > Constants.PhysicsBodyCorrectionDistance
+        c.params.currentAreaId == areaId &&
+          creatureBodies.contains(c.id) &&
+          creatureBodies(c.id).pos.distance(c.pos) > Constants.PhysicsBodyCorrectionDistance
       ) {
-        creatureBodies(creature.id).setPos(creature.pos)
+        creatureBodies(c.id).setPos(c.pos)
       }
-    )
-  }
-
-  def teleportIfInArea(
-      creatureId: GameEntityId[Creature],
-      pos: Vector2f,
-      areaId: AreaId,
-      game: CoreGame
-  ): Unit = {
-    if (
-      game.gameState.creatures.contains(creatureId) &&
-      game.gameState.creatures(creatureId).params.currentAreaId == areaId
-    ) {
-      creatureBodies(creatureId).setPos(pos)
     }
   }
 
-  def setSensorIfInArea(
-      creatureId: GameEntityId[Creature],
-      areaId: AreaId,
-      game: CoreGame
-  ): Unit = {
-    if (
-      game.gameState.creatures.contains(creatureId) &&
-      game.gameState.creatures(creatureId).params.currentAreaId == areaId
-    ) {
-      creatureBodies(creatureId).setSensor()
+  def teleportIfInArea(id: GameEntityId[Creature], pos: Vector2f, areaId: AreaId, game: CoreGame): Unit =
+    if (game.gameState.creatures.get(id).exists(_.params.currentAreaId == areaId)) {
+      creatureBodies(id).setPos(pos)
     }
-  }
 
-  def setNonSensorIfInArea(
-      creatureId: GameEntityId[Creature],
-      areaId: AreaId,
-      game: CoreGame
-  ): Unit = {
-    if (
-      game.gameState.creatures.contains(creatureId) &&
-      game.gameState.creatures(creatureId).params.currentAreaId == areaId
-    ) {
-      creatureBodies(creatureId).setNonSensor()
+  def setSensorIfInArea(id: GameEntityId[Creature], areaId: AreaId, game: CoreGame): Unit =
+    if (game.gameState.creatures.get(id).exists(_.params.currentAreaId == areaId)) {
+      creatureBodies(id).setSensor()
     }
-  }
+
+  def setNonSensorIfInArea(id: GameEntityId[Creature], areaId: AreaId, game: CoreGame): Unit =
+    if (game.gameState.creatures.get(id).exists(_.params.currentAreaId == areaId)) {
+      creatureBodies(id).setNonSensor()
+    }
 }
