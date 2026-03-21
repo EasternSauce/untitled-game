0e734e2cffa7aa11a121f09e33c81f8da0e34069
EasternSauce <frequentmerger@gmail.com>
2026-03-21 21:45:14 +0100

    concrete projectiles and effects

--- src/main/scala/com/easternsauce/game/gamestate/effect/EffectComponent.scala
+++ src/main/scala/com/easternsauce/game/gamestate/effect/EffectComponent.scala
@@ -2,10 +2,27 @@ package com.easternsauce.game.gamestate.effect
 
 import com.easternsauce.game.entitycreator.EffectComponentType
 import com.easternsauce.game.gamestate.GameEntity
+import com.easternsauce.game.gamestate.id.AreaId
 import com.easternsauce.game.gamestate.id.GameEntityId
 
 trait EffectComponent extends GameEntity {
   def id: GameEntityId[EffectComponent]
   def effectType: EffectComponentType
   def params: EffectComponentParams
+  def currentAreaId: AreaId
+
+  def update(delta: Float): EffectComponent
+}
+
+case class BuffEffectComponent(
+    id: GameEntityId[EffectComponent],
+    effectType: EffectComponentType,
+    params: EffectComponentParams,
+    currentAreaId: AreaId
+) extends EffectComponent {
+  override def update(delta: Float): EffectComponent = {
+    val newTime = params.timeElapsed + delta
+    val expired = newTime >= params.duration
+    this.copy(params = params.copy(timeElapsed = newTime, isScheduledToBeRemoved = expired))
+  }
 }
--- src/main/scala/com/easternsauce/game/gamestate/projectile/ProjectileComponent.scala
+++ src/main/scala/com/easternsauce/game/gamestate/projectile/ProjectileComponent.scala
@@ -1,11 +1,28 @@
 package com.easternsauce.game.gamestate.projectile
 
-import com.easternsauce.game.entitycreator.ProjectileComponentType
 import com.easternsauce.game.gamestate.GameEntity
+import com.easternsauce.game.gamestate.id.AreaId
 import com.easternsauce.game.gamestate.id.GameEntityId
 
 trait ProjectileComponent extends GameEntity {
   def id: GameEntityId[ProjectileComponent]
-  def projectileType: ProjectileComponentType
+  def abilityId: GameEntityId[_]
   def params: ProjectileComponentParams
+  def currentAreaId: AreaId
+
+  def update(delta: Float): ProjectileComponent
+}
+
+case class BasicProjectileComponent(
+    id: GameEntityId[ProjectileComponent],
+    abilityId: GameEntityId[_],
+    params: ProjectileComponentParams,
+    currentAreaId: AreaId
+) extends ProjectileComponent {
+  override def update(delta: Float): ProjectileComponent = {
+    // simple linear motion update
+    val (x, y) = params.pos
+    val (vx, vy) = params.velocity
+    this.copy(params = params.copy(pos = (x + vx * delta, y + vy * delta)))
+  }
 }
