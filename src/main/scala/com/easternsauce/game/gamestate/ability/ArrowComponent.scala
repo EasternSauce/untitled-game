8f3d762cb0f445efa629f2946d80c06d44c5aa96
EasternSauce <frequentmerger@gmail.com>
2026-03-21 13:48:36 +0100

    arrow max range

--- src/main/scala/com/easternsauce/game/gamestate/ability/ArrowComponent.scala
+++ src/main/scala/com/easternsauce/game/gamestate/ability/ArrowComponent.scala
@@ -1,10 +1,16 @@
 package com.easternsauce.game.gamestate.ability
 
+import com.easternsauce.game.core.CoreGame
 import com.easternsauce.game.gamestate.creature.FramesDefinition
+import com.easternsauce.game.math.Vector2f
+import com.softwaremill.quicklens.ModifyPimp
 
 case class ArrowComponent(params: AbilityComponentParams) extends AbilityComponent {
-  override val textureFileName: String = "arrow"
 
+  // --- Constant max range ---
+  val maxRange: Float = 30f // tweak this value as needed
+
+  override val textureFileName: String = "arrow"
   override val textureSize: Int = 64
   override val bodyRadius: Float = 0.3f
 
@@ -13,16 +19,30 @@ case class ArrowComponent(params: AbilityComponentParams) extends AbilityCompone
 
   override val speed: Float = 12f
 
-  override def init(): ArrowComponent = {
-    this
-  }
+  override def init(): ArrowComponent = this
 
   override def isDestroyedOnCreatureContact: Boolean = true
-
   override def isDestroyedOnTerrainContact: Boolean = true
 
-  override def copy(params: AbilityComponentParams): AbilityComponent = {
-    ArrowComponent(params)
+  override def update(
+                       delta: Float,
+                       newPos: Option[Vector2f]
+                     )(implicit game: CoreGame): AbilityComponent = {
+
+    val updated = super.update(delta, newPos)
+
+    val distanceFromSpawn = updated.params.pos.distance(updated.params.spawnPos)
+
+    if (distanceFromSpawn > maxRange && !updated.params.isScheduledToBeRemoved) {
+      updated
+        .modify(_.params.isScheduledToBeRemoved)
+        .setTo(true)
+        .modify(_.params.isContinueScenario)
+        .setTo(false)
+    } else updated
   }
+
+  override def copy(params: AbilityComponentParams): AbilityComponent =
+    ArrowComponent(params)
 
 }
